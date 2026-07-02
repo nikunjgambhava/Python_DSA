be3be725d947459980912fe6d76f6323aa0d79ef
Nikunj Gambhava <gambhavanikunj03@gmail.com>
2026-07-02 19:23:21 +0530

    Create 2.Selection_Sort.py

---
+++ 2.Selection_Sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+# Example
+marks = [78, 55, 89, 92, 67]
+print("Selection Sort:", selection_sort(marks))
